5188e3d48d6cf27ba218ccd7d4e059c6e48f96c3
CSE-A23 <60130578+CSE-A23@users.noreply.github.com>
2020-01-21 17:26:30 +0530

    Wordchange.c

---
+++ Wordchange.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+  int n,m,i;
+  char arr[100];
+  clrscr();
+  printf("Enter the character:");
+  scanf("%d",arr);
+  n=strlen(a);
+  if(n%2!=0)
+  {
+    m=n-1;
+    arr[m]='O';
+    arr[m-1]='O';
+  }
+  for(i=0;i<n;i++)
+  {
+    if(a[i]=='a')
+    a[i]='u';
+    if(a[i]=='i')
+    a[i]='e';
+  }
+   printf("After changing %s",a);
+   getch();
+}
+   
+  
